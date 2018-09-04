c5f6d13e5ea2fce9d7a234d2ec1ac1361da3da70
Fangzheng Zhang <32881702+FangzhengZhang@users.noreply.github.com>
2018-09-03 22:56:15 -0400

    Functions Format
    functions format in python

---
+++ Functions.py
@@ -0,0 +1,18 @@
+# functions in python
+
+# all the functions begin with a 'def' + 'name' + '():'
+# and inside the function, code begin after a TAB
+def say_hi():
+    print("Hi")
+    print("all the functions begin with a 'def' + 'name' + '():'\n" +
+          "and inside the function, code begin after a TAB")
+
+# call the function
+say_hi()
+
+
+# functions with parameter
+def say_hi_to(input_name):
+    print("Hi " + input_name)
+
+say_hi_to("Fang")
